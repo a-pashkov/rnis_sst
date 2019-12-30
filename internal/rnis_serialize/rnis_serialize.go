411b1e2cd780a2c9cae79cc0b6bf754ad30856ff
Aleksey Pashkov <a-pashkov@mail.ru>
2019-12-30 03:03:59 +0300

    data types added

--- internal/rnis_serialize/rnis_serialize.go
+++ internal/rnis_serialize/rnis_serialize.go
@@ -2,21 +2,22 @@ package rnis_serialize
 
 import (
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"math"
 	"strconv"
 )
 
 const (
-	tNull      = 0
-	tBinary255 = 1
-	// tBinary65535 = 2
-	tUint1 = 10
-	// tSint1 = 20
-	// tSint4 = 22
-	tSint8    = 23
-	tFloat8   = 31
-	delimiter = ";"
+	tNull        = 0
+	tBinary255   = 1
+	tBinary65535 = 2
+	tUint1       = 10
+	tSint1       = 20
+	tSint4       = 22
+	tSint8       = 23
+	tFloat8      = 31
+	delimiter    = ";"
 )
 
 /*
@@ -50,14 +51,17 @@ type sRnisBinary255 struct {
 }
 
 func (rnisBinary255 *sRnisBinary255) String() string {
-	return string(rnisBinary255.name) + delimiter + string(rnisBinary255.value)
+	return string(rnisBinary255.name) + delimiter + "x" + hex.EncodeToString(rnisBinary255.value)
 }
 
 // tBinary65535
-type rnisBinary65535 struct{}
+type sRnisBinary65535 struct {
+	name  []byte
+	value []byte
+}
 
-func (_ *rnisBinary65535) String() string {
-	return ""
+func (rnisBinary65535 *sRnisBinary65535) String() string {
+	return string(rnisBinary65535.name) + delimiter + "x" + hex.EncodeToString(rnisBinary65535.value)
 }
 
 // tUint1
@@ -71,17 +75,23 @@ func (rnisUint *sRnisUint) String() string {
 }
 
 // tSint1
-type rnisSint1 struct{}
+type sRnisSint1 struct {
+	name  []byte
+	value int8
+}
 
-func (_ *rnisSint1) String() string {
-	return ""
+func (rnisSint1 *sRnisSint1) String() string {
+	return string(rnisSint1.name) + delimiter + strconv.Itoa(int(rnisSint1.value))
 }
 
 // tSint4
-type rnisSint4 struct{}
+type sRnisSint4 struct {
+	name  []byte
+	value int32
+}
 
-func (_ *rnisSint4) String() string {
-	return ""
+func (rnisSint4 *sRnisSint4) String() string {
+	return string(rnisSint4.name) + delimiter + strconv.Itoa(int(rnisSint4.value))
 }
 
 // tSint8
@@ -101,7 +111,6 @@ type sRnisFloat8 struct {
 }
 
 func (rnisFloat8 *sRnisFloat8) String() string {
-	//fmt.Println(strconv.FormatFloat(rnisFloat8.value, 'f', -1, 64))
 	return string(rnisFloat8.name) + delimiter + strconv.FormatFloat(rnisFloat8.value, 'f', 6, 64)
 }
 
@@ -120,23 +129,46 @@ func Deserialize(b []byte) (records []RnisRecord, err error) {
 		vType := b[0]
 		b = b[1:]
 
-		//fmt.Println(vType)
 		switch vType {
+		case tNull:
+			records = append(records, &sRnisNull{name: name})
+		case tBinary255:
+			size := uint8(b[0])
+			b = b[1:]
+			v := b[:size]
+			b = b[size:]
+			records = append(records, &sRnisBinary255{name: name, value: v})
+		case tBinary65535:
+			size := binary.BigEndian.Uint16(b[:2])
+			b = b[2:]
+			v := b[:size]
+			b = b[size:]
+			records = append(records, &sRnisBinary65535{name: name, value: v})
+
 		case tUint1:
 			v := uint8(b[0])
 			b = b[1:]
 			records = append(records, &sRnisUint{name: name, value: v})
-		case tFloat8:
-			bits := binary.BigEndian.Uint64(b[:8])
-			b = b[8:]
-			v := math.Float64frombits(bits)
-			records = append(records, &sRnisFloat8{name: name, value: v})
+		case tSint1:
+			v := int8(b[0])
+			b = b[1:]
+			records = append(records, &sRnisSint1{name: name, value: v})
+		case tSint4:
+			bits := binary.BigEndian.Uint32(b[:4])
+			b = b[4:]
+			v := int32(bits)
+			records = append(records, &sRnisSint4{name: name, value: v})
 		case tSint8:
 			bits := binary.BigEndian.Uint64(b[:8])
 			b = b[8:]
 			v := int64(bits)
 			records = append(records, &sRnisSint8{name: name, value: v})
 
+		case tFloat8:
+			bits := binary.BigEndian.Uint64(b[:8])
+			b = b[8:]
+			v := math.Float64frombits(bits)
+			records = append(records, &sRnisFloat8{name: name, value: v})
 		default:
 			return nil, fmt.Errorf("'%s' has unknown type %s\n", string(name), strconv.Itoa(int(vType)))
 		}
