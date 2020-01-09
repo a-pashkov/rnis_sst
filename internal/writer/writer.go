4206ed69df2625cd4ad287fe44149fbd69d3728c
Aleksey Pashkov <a-pashkov@mail.ru>
2020-01-09 21:51:15 +0300

    compress buffer

--- internal/writer/writer.go
+++ internal/writer/writer.go
@@ -6,10 +6,12 @@ import (
 	"os"
 	"path"
 	//"io"
+	"bufio"
+	"compress/gzip"
 )
 
 const (
-	ext          = ".csv"
+	ext          = ".csv.gz"
 	maxOpenFiles = 10000
 )
 
@@ -18,11 +20,27 @@ type CsvRecord struct {
 	Line string
 }
 
+type wGz struct {
+	f  *os.File
+	gf *gzip.Writer
+	fw *bufio.Writer
+}
+
 // Считывает данные для записи
 // Получает path, in, finished
 func InitWriter(path string, in <-chan CsvRecord, finished chan<- struct{}) {
 	// map хранит пары id:fileDescroptor
-	o := make(map[int]*os.File)
+	o := make(map[int]wGz)
+
+	defer func() {
+		for _, f := range o {
+			err := closeGz(f)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+		finished <- struct{}{}
+	}()
 
 	for rec := range in {
 		// Получить дескриптор для записи
@@ -31,65 +49,76 @@ func InitWriter(path string, in <-chan CsvRecord, finished chan<- struct{}) {
 			panic(err)
 		}
 		// Запись в файл
-		_, err = f.WriteString(rec.Line + "\n")
+		_, err = (f.fw).WriteString(rec.Line + "\n")
 		if err != nil {
 			panic(err)
 		}
-		//fmt.Println(f, err, len(o))
 	}
-
-	// Закрыть файлы
-	for _, f := range o {
-		fileClose(f)
-	}
-
-	// Сообщение о завершении
-	finished <- struct{}{}
 }
 
 // Открывает файл и создаёт путь к нему
 // Возвращает файл
-func getFile(o map[int]*os.File, p string, id int) (f *os.File, err error) {
+func getFile(o map[int]wGz, p string, id int) (f wGz, err error) {
 	// Если id есть в map возвращаем его дескриптор
-	if o[id] != nil {
-		return o[id], nil
+	if f, ok := o[id]; ok {
+		return f, nil
 	}
 
 	// удаляем лишние ключи из map
-	for i, f := range o {
+	for iD, fD := range o {
 		if len(o) >= maxOpenFiles {
 			// fmt.Println("закрыть файл, превышен буфер", i, f)
 			// закрыть файл
-			err := f.Close()
+			err = closeGz(fD)
 			if err != nil {
-				return nil, err
+				return
 			}
 			// удалить из отображения
-			delete(o, i)
+			delete(o, iD)
+		} else {
+			break
 		}
 	}
 
 	idStr := fmt.Sprintf("%07d", id)
 	dir := path.Join(p, idStr[0:1], idStr[1:2], idStr[2:3])
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err := os.MkdirAll(dir, 0755)
+	if _, err = os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
 		if err != nil {
-			return nil, err
+			return
 		}
 	}
 
 	full := path.Join(dir, idStr+ext)
 
-	f, err = os.OpenFile(full, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// Открытие файла
+	f.f, err = os.OpenFile(full, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return
+	}
+	// gzip writer
+	f.gf = gzip.NewWriter(f.f)
+	// buffer
+	f.fw = bufio.NewWriter(f.gf)
+
 	o[id] = f
-	return f, err
+	return
 }
 
-// Закрытие файла
-func fileClose(f *os.File) {
-	name := f.Name()
-	err := f.Close()
+// Сброс буфера, закрытие gzip, закрытие файла
+func closeGz(f wGz) (err error) {
+	name := f.f.Name()
+	err = f.fw.Flush()
 	if err != nil {
-		fmt.Println(name, err)
+		return fmt.Errorf("%s\n%s", name, err.Error())
 	}
+	err = f.gf.Close()
+	if err != nil {
+		return fmt.Errorf("%s\n%s", name, err.Error())
+	}
+	err = f.f.Close()
+	if err != nil {
+		return fmt.Errorf("%s\n%s", name, err.Error())
+	}
+	return
 }
